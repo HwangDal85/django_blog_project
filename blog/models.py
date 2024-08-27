b6fb0183e7f8e8966b2ead771660155f49e5f3d8
Seong-jae.Park <bvc3435@gamil.com>
2024-08-27 14:52:41 +0900

    ListView 기능 재생성(FBV)

--- blog/models.py
+++ blog/models.py
@@ -14,8 +14,8 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     # author = models.ForeignKey(User, )
     
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from .models import Post
+from django.core.paginator import Paginator
 
 def home(request):
     recent_posts = Post.objects.order_by('-created_date')[:5]
     return render(request, 'blog/home.html', {'recent_posts': recent_posts})
+
+def post_list(request):
+    post_list = Post.objects.all().order_by('-created_date')
+    paginator = Paginator(post_list, 10)  # 페이지당 10개 게시글
+
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+
+    return render(request, 'blog/post_list.html', {'posts': posts})
