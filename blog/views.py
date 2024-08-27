71de30cfafdb83799562135874f4357794ae579f
Seong-jae.Park <bvc3435@gamil.com>
2024-08-27 15:00:44 +0900

    상세 페이지 내용 작성

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator
 
@@ -14,3 +14,7 @@ def post_list(request):
     posts = paginator.get_page(page)
 
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
