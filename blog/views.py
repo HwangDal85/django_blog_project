75f334d5213ac2da01be10d74971c780ca89bdc2
Seong-jae.Park <bvc3435@gamil.com>
2024-08-27 11:27:37 +0900

    forms, views 작성

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+# blog/views.py
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from .models import Post
+from .forms import PostForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'post_list'
+
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy('blog:list')
+
+
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    context_object_name = 'post_update'
+    form_class = PostForm
+    success_url = reverse_lazy('blog:list')
+
+
+class PostDeleteView(DeleteView):
+    model = Post
