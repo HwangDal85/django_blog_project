1003354862b93642d1577513c17e38c616664aeb
Seong-jae.Park <bvc3435@gamil.com>
2024-08-27 11:10:20 +0900

    views, forms, urls, models 전면수정

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+# class User(models.Model):
+#     pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # author = models.ForeignKey(User, )
+    
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,32 +1,3 @@
-# blog/views.py
-from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
-from .models import Post
-from .forms import PostForm
-from django.urls import reverse_lazy
+from django.shortcuts import render
 
-
-class PostListView(ListView):
-    model = Post
-    context_object_name = 'post_list'
-
-
-class PostCreateView(CreateView):
-    model = Post
-    form_class = PostForm
-    success_url = reverse_lazy('blog:list')
-
-
-class PostDetailView(DetailView):
-    model = Post
-    context_object_name = 'post'
-
-
-class PostUpdateView(UpdateView):
-    model = Post
-    context_object_name = 'post_update'
-    form_class = PostForm
-    success_url = reverse_lazy('blog:list')
-
-
-class PostDeleteView(DeleteView):
-    model = Post
+# Create your views here.
