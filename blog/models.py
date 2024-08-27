0ec67e78b40c70c82ce4cf86b2fbc4f804935563
Seong-jae.Park <bvc3435@gamil.com>
2024-08-27 11:11:54 +0900

    models 수정 및 migrate

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    description = models.CharField(blank=True)
+    description = models.TextField()
 
     def __str__(self):
         return self.name
