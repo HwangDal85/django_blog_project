00db6ee17f8a6d923c399b135d184186883852bf
Seong-jae.Park <bvc3435@gamil.com>
2024-08-28 17:52:46 +0900

    이후 추가할거 미리 주석처리

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,25 @@ class Post(models.Model):
     updated_date = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    # tags = models.ManyToManyField("Tag", blank=True)
+    # view_count = models.IntegerField(default=0)
     
     def __str__(self):
         return self.title
+    
+
+# class Comment(models.Model):
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
+#     message = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+#     def __str__(self):
+#         return self.message
+
+# class Tag(models.Model):
+#     name = models.CharField(max_length=50, unique=True)
+
+#     def __str__(self):
+#         return self.name
