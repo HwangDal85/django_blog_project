831cc899c7806456d4e4a0b0dc12e8ffc0e33479
Seong-jae.Park <bvc3435@gamil.com>
2024-08-30 09:14:07 +0900

    게시글 이미지 업로드 기능 추가

--- blog/forms.py
+++ blog/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'category']
+        fields = ['title', 'content', 'category', 'image']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -17,3 +19,6 @@ urlpatterns = [
     path('profile/update/', views.user_profile_update, name='user_profile_update'), 
     path('profile/pass_update/', views.change_password, name='user_pass_update'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
