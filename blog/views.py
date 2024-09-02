45de696b7863593c083bc4b25cb03134817ac384
Seong-jae.Park <bvc3435@gamil.com>
2024-09-02 12:21:31 +0900

    사용 안하는 모듈 삭제

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, FormView, TemplateView, RedirectView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
